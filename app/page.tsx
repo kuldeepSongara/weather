ded85935d04b8c2061391254c625982717b40840
Kuldeep Songara <kuldeepsongaraz@gmail.com>
2024-03-27 11:54:54 +0530

    app setup

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Weaather app - Dev in progess
+    </main>
+  );
+}
